01d3f7807de89e0b7bcd73d3678361b0652368d1
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-09-12 16:56:26 +0100

    hashing the password from req.body with bscrypt,

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,17 @@
-
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequest} = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async (req,res)=>{
-    res.send('register users')
+
+    const user = await User.create({...req.body}) 
+    const {_id,name} = user
+
+    const token = jwt.sign({userId:_id,name:name},'jwtSecret',{expiresIn:'30d'})
+    res.status(StatusCodes.CREATED).json({user:{user:name},token})
+
+
 }
 
 const login = async (req,res)=>{
