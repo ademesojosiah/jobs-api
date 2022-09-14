b498f17c3c17eb68c99c7ea00d41a896c6da4130
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-09-14 13:43:35 +0100

    updating authentication middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -18,7 +18,7 @@ const auth = async (req,res,next) =>{
     const payload = jwt.verify(token,process.env.JWT_SECRET)
     //attach the user to the job routes
     req.user = {userId:payload.userId, name:payload.name}
-    next()
+    next(); 
   } catch (error) {
     throw new UnAuthenticatedError('Authentication invalid')
     
