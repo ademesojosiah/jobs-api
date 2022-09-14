2f469e7d0aa38f42366d44358e9f7f0de4a86e10
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-09-14 18:11:35 +0100

    updated a createJob and a get all job controller

--- controllers/job.js
+++ controllers/job.js
@@ -1,14 +1,19 @@
-
+const Job = require('../models/Jobs')
+const {StatusCodes} = require('http-status-codes')
+const  {BadRequest,NotFoundError,} = require('../errors')
 
 const getAllJobs = async (req,res)=>{
-    res.send('get all jobs')
+    const jobs = await job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count:jobs.length})
 }
 
 const getJob = async (req,res)=>{
     res.send('get single jobs')
 }
 const createJob = async (req,res)=>{
-    res.send('create jobs')
+    req.body.createdBy = req.user.userId 
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json(job)
 }
 const updateJob = async (req,res)=>{
     res.send('update jobs')
