8ac7f0a03d3b8aa43604428347e8608459a09abd
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-09-15 04:17:08 +0100

    updated get singleJob Id

--- controllers/job.js
+++ controllers/job.js
@@ -3,12 +3,14 @@ const {StatusCodes} = require('http-status-codes')
 const  {BadRequest,NotFoundError,} = require('../errors')
 
 const getAllJobs = async (req,res)=>{
-    const jobs = await job.find({createdBy:req.user.userId}).sort('createdAt')
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
     res.status(StatusCodes.OK).json({jobs, count:jobs.length})
 }
 
 const getJob = async (req,res)=>{
-    res.send('get single jobs')
+    const {user:{userId},params:{id:jobId}  } = req
+    const job = await Job.findOne({_id:jobId,createdBy:userId})
+    !job && new BadRequest(`no job with id ${jobId}`)
 }
 const createJob = async (req,res)=>{
     req.body.createdBy = req.user.userId 
